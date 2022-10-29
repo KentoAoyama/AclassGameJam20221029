84d4a6df26745c27c9eefc1eb248e57f0c453a19
Ooyamashun0724 <shun20010724@icloud.com>
2022-10-29 14:41:06 +0900

    フォント追加

--- Assets/Oyama/scripts/HowtoPlay.cs
+++ Assets/Oyama/scripts/HowtoPlay.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class HowtoPlay : MonoBehaviour
 {
     [SerializeField] GameObject HowToPlaypanel;
-    // Start is called before the first frame update
     public void Openpanel()
     {
         HowToPlaypanel.SetActive(true);
+    }
+    public void Closepanel()
+    {
+        HowToPlaypanel.SetActive(false);
     }
 }
